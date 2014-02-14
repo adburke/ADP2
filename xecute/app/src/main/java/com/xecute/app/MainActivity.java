d1bac936476c88ba7dbde06964d4be285505d7ee
adburke <adburke@fullsail.edu>
2014-02-13 20:12:05 -0600

    Created app icon - Created Project Fragment Dialogs - Created Navigation Spinner - Implemented a Log Out in the over flow

--- xecute/app/src/main/java/com/xecute/app/MainActivity.java
+++ xecute/app/src/main/java/com/xecute/app/MainActivity.java
@@ -10,23 +10,49 @@
 
 package com.xecute.app;
 
+import android.app.ActionBar;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
-public class MainActivity extends FragmentActivity {
+import com.parse.ParseUser;
+
+public class MainActivity extends FragmentActivity implements ActionBar.OnNavigationListener {
 
     Context mContext;
+
+    ActionBar actionBar;
+    Spinner navSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mContext = this;
+
+        actionBar = getActionBar();
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+
+
+        navSpinner = new Spinner(mContext);
+        SpinnerAdapter mSpinnerAdapter = ArrayAdapter.createFromResource(mContext, R.array.action_list,
+                android.R.layout.simple_spinner_dropdown_item);
+        actionBar.setListNavigationCallbacks(mSpinnerAdapter, this);
 
         FragmentManager fragManager = getSupportFragmentManager();
         FragmentTransaction fragTrans = fragManager.beginTransaction();
@@ -38,19 +64,66 @@ public class MainActivity extends FragmentActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        
+
         switch (item.getItemId()) {
             case R.id.action_logout:
                 Log.i("MAIN", "Log Out Selected.");
+                ParseUser.logOut();
+                Intent intent = new Intent(mContext, LoginActivity.class);
+                startActivity(intent);
+                // Removes activity from the stack so we can not navigate back
+                finish();
                 return true;
 
             case R.id.action_add_project:
             Log.i("MAIN", "New Project Selected.");
+
+                AlertDialog.Builder projectBuilder = new AlertDialog.Builder(this);
+                projectBuilder.setTitle(R.string.action_new_project);
+                // Get the layout inflater
+                LayoutInflater inflater = this.getLayoutInflater();
+
+                // Inflate and set the layout for the dialog
+                // Pass null as the parent view because its going in the dialog layout
+                projectBuilder.setView(inflater.inflate(R.layout.create_project, null))
+                        // Add action buttons
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                // sign in the user ...
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                            }
+                        });
+                projectBuilder.create().show();
+
             return true;
 
             case R.id.action_filter_project:
                 Log.i("MAIN", "Filter Project Selected.");
+
+                AlertDialog.Builder filterBuilder = new AlertDialog.Builder(mContext);
+                filterBuilder.setTitle(R.string.action_filter_project)
+                        .setItems(R.array.project_filters, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // The 'which' argument contains the index position
+                                // of the selected item
+                            }
+                        });
+                filterBuilder.create().show();
+
                 return true;
         }
 
@@ -58,4 +131,14 @@ public class MainActivity extends FragmentActivity {
     }
 
 
+    @Override
+    public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+        if (itemPosition == 0) {
+            Log.i("MAIN", "Projects Selected");
+
+        } else {
+            Log.i("MAIN", "My Tasks Selected");
+        }
+        return false;
+    }
 }
